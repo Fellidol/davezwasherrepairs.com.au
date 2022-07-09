f492f1f02696b8835b50c02d93bc1d657c6dae6b
richhdev <79666708+richhdev@users.noreply.github.com>
2022-07-09 21:17:59 +1000

    init smoothscroll polyfill

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
+import smoothscroll from "smoothscroll-polyfill";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/600.css";
 import "@fontsource/montserrat/800.css";
-
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    smoothscroll.polyfill();
+  });
   return <Component {...pageProps} />;
 }
 
