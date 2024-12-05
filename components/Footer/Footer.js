27e4be1f22dc0568ea5fba7729765a219a44dc93
richhdev <79666708+richhdev@users.noreply.github.com>
2024-12-05 13:56:14 +1100

    update copyright year in footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,14 +1,14 @@
-import Text from "../Text";
 import { Outer, Inner, Title, TextContainer } from "./styled";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <Outer>
       <Inner>
         <Title>We accept Cash, Cheque or Electronic Transfer</Title>
         <TextContainer>
           <p>
-            © Copyright 2022 - Davez Washer &amp; Dryer Repairs - Call Dave on{" "}
+            © Copyright {year} - Davez Washer &amp; Dryer Repairs - Call Dave on{" "}
             <a href="tel:0452 234 752">0452 234 752</a> - Melbourne
           </p>
         </TextContainer>
